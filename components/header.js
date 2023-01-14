898db9cb69f501db0622be204e01bdf7cccc9ac1
Shenita Rose <89140305+ShenitaRose@users.noreply.github.com>
2023-01-14 16:13:50 +0000

    edited the promo component

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="bg-gray-50">
         <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
             <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-                <input type="text" className="input-text" placeholder="Search..." />
+                
             </div>
             <div className="shrink w-80 sm:order-2">
                 <Link href="/" className="font-bold uppercase text-3xl">The Holiday Cafe</Link>
