9f026cad2079120f963fdcd58db9bfe1711944b6
ShenitaRose <simsationelle@gmail.com>
2023-01-19 11:31:07 +0000

    remove card and promo

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,15 @@ import Format from '../layout/format'
 
 // Component Imports
 import Hero from '../components/Hero'
-import Card from '../components/card'
-import Promo from '../components/promo'
+
 
 export default function Home() {
   return (
       
       <Format>
         <Hero />
-        <Promo />
-        <Card />
+        
+        
         
       </Format>
 
